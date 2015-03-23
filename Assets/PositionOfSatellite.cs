46c053758b7f43021e733b4d3565bec8e56224b0
hira-yahoo <misocasu@yahoo.co.jp>
2015-03-23 19:37:31 +0900

    add a class to contain coordinates.

---
+++ Assets/PositionOfSatellite.cs
@@ -0,0 +1,23 @@
+﻿public class PositionOfSatellite {
+
+	private float angle;
+	private float distance;
+
+	PositionOfSatellite() {
+		set (0.0f, 0.0f);
+	}
+
+	public void set(float angle, float distance) {
+		this.angle = angle;
+		this.distance = distance;
+	}
+
+	public float getAngle() {
+		return this.angle;
+	}
+
+	public float getDistance() {
+		return this.distance;
+	}
+
+}
