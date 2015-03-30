d19ed5227ef5c4742312add5ecd4777585133f81
hira-yahoo <misocasu@yahoo.co.jp>
2015-03-30 18:08:35 +0900

    rewrite an inner class.

--- Assets/scripts/satellites/SatelliteController.cs
+++ Assets/scripts/satellites/SatelliteController.cs
@@ -176,4 +176,38 @@ public class SatelliteController : MonoBehaviour {
 
 	//	private void focusOnTheStarOnRelativeX () {
 	//	}
+
+	class PositionOfSatellite {
+		
+		private float horizontalAngle;
+		private float verticalAngle;
+		private float distance;
+		
+		public PositionOfSatellite(float horizontalAngle, float verticalAngle, float distance) {
+			this.horizontalAngle = horizontalAngle;
+			this.verticalAngle = verticalAngle;
+			setDistance (distance);
+		}
+		
+		public float getHorizontalAngle() {
+			return this.horizontalAngle;
+		}
+		
+		public float getVerticalAngle() {
+			return this.verticalAngle;
+		}
+		
+		public void increaseHorizontalAngle(float angle) {
+			this.horizontalAngle += angle;
+		}
+		
+		public void setDistance(float distance) {
+			this.distance = distance;
+		}
+		
+		public float getDistance() {
+			return this.distance;
+		}
+	}
+
 }
